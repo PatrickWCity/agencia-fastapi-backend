cf55e1ccc7f5967f07480e0f8d28c0f9c7ebbb02
Patrick Ciudad <PatrickWCity@gmail.com>
2024-12-24 19:22:58 -0300

    style(fastapi): updated project file structure

--- src/app/main.py
+++ src/app/main.py
@@ -1,27 +1,21 @@
-from typing import Union
+from fastapi import Depends, FastAPI
 
-from fastapi import FastAPI
-from pydantic import BaseModel
+from .dependencies import get_query_token, get_token_header
+from .internal import admin
+from .routers import items, users
 
-app = FastAPI()
+app = FastAPI(dependencies=[Depends(get_query_token)])
 
-
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
-
+app.include_router(users.router)
+app.include_router(items.router)
+app.include_router(
+    admin.router,
+    prefix="/admin",
+    tags=["admin"],
+    dependencies=[Depends(get_token_header)],
+    responses={418: {"description": "I'm a teapot"}},
+)
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+async def root():
+    return {"message": "Hello Bigger Applications!"}
