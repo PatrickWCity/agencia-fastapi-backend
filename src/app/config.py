c5c5b6adbd7f12fbd284e0516262b662f28d352c
Patrick Ciudad <PatrickWCity@gmail.com>
2024-12-25 15:30:09 -0300

    feat(pydantic-settings): updated Settings and Environment Variables

--- src/app/config.py
+++ src/app/config.py
@@ -3,7 +3,19 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     app_name: str = "Awesome API"
+    app_env: str = "local"
+
     admin_email: str
     items_per_user: int = 50
 
-    model_config = SettingsConfigDict(env_file=".env")
+    db_connection: str = "sqlite"
+    db_host: str = ""
+    db_port: int = None
+    db_database: str = "/database.db"
+    db_username: str = ""
+    db_password: str = ""
+
+    model_config = SettingsConfigDict(
+        env_file=(".env.example", ".env.local", ".env"),
+        extra="ignore",
+    )
--- src/app/info.py
+++ src/app/info.py
@@ -3,6 +3,8 @@ from typing import Annotated
 
 from fastapi import Depends, FastAPI
 
+from pydantic_core import MultiHostUrl
+
 from .config import Settings
 
 app = FastAPI()
@@ -17,6 +19,23 @@ def get_settings():
 async def info(settings: Annotated[Settings, Depends(get_settings)]):
     return {
         "app_name": settings.app_name,
+        "app_env": settings.app_env,
         "admin_email": settings.admin_email,
         "items_per_user": settings.items_per_user,
+        "db_connection": settings.db_connection,
+        "db_host": settings.db_host,
+        "db_port": settings.db_port,
+        "db_database": settings.db_database,
+        "db_username": settings.db_username,
+        "db_password": settings.db_password,
+        "db_url": str(
+            MultiHostUrl.build(
+                scheme=settings.db_connection,
+                host=settings.db_host,
+                port=settings.db_port or None,
+                path=settings.db_database,
+                username=settings.db_username,
+                password=settings.db_password,
+            )
+        ),
     }
