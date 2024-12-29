8e2365adf66f75ed400692fab747bac6d88e64a3
Patrick Ciudad <PatrickWCity@gmail.com>
2024-12-29 19:04:01 -0300

    feat(sqlmodel): updated hero model with powers and weapon

--- src/app/models/hero.py
+++ src/app/models/hero.py
@@ -3,7 +3,9 @@ from datetime import datetime, timezone
 from sqlmodel import Field, Relationship, SQLModel
 
 if TYPE_CHECKING:
+    from app.models.power import Power, PowerPublic
     from app.models.team import Team, TeamPublic
+    from app.models.weapon import Weapon, WeaponPublic
 
 
 class HeroBase(SQLModel):
@@ -24,12 +26,17 @@ class HeroBase(SQLModel):
     )
 
     team_id: Optional[int] = Field(default=None, foreign_key="team.id")
+    weapon_id: Optional[int] = Field(default=None, foreign_key="weapon.id")
 
 
 class Hero(HeroBase, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
 
     team: Optional["Team"] = Relationship(back_populates="heroes")
+
+    weapon: Optional["Weapon"] = Relationship(back_populates="heroes")
+
+    powers: list["Power"] = Relationship(back_populates="hero")
 
 
 class HeroPublic(HeroBase):
@@ -49,14 +56,25 @@ class HeroUpdate(SQLModel):
     deleted_at: Optional[datetime] = None
 
     team_id: Optional[int] = None
+    weapon_id: Optional[int] = None
 
 
-class HeroPublicWithTeam(HeroPublic):
+class HeroPublicWithPowersTeamWeapon(HeroPublic):
     team: Optional["TeamPublic"] = None
+    weapon: Optional["WeaponPublic"] = None
+    powers: List["PowerPublic"] = []
 
+
+# fix 'PydanticUndefinedAnnotation: name 'PowerPublic' is not defined' error
+# see: https://github.com/tiangolo/sqlmodel/discussions/757
+from app.models.power import PowerPublic
 
 # fix 'PydanticUndefinedAnnotation: name 'TeamPublic' is not defined' error
 # see: https://github.com/tiangolo/sqlmodel/discussions/757
 from app.models.team import TeamPublic
 
-HeroPublicWithTeam.model_rebuild()
+# fix 'PydanticUndefinedAnnotation: name 'WeaponPublic' is not defined' error
+# see: https://github.com/tiangolo/sqlmodel/discussions/757
+from app.models.weapon import WeaponPublic
+
+HeroPublicWithPowersTeamWeapon.model_rebuild()
